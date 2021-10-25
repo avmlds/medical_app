2577f190a9a96ab7928c7ee2cbd3b8bb55cc2857
alexander <alexander.lopatnikov@lumiprobe.com>
2021-10-25 21:42:44 +0300

    Cors middleware updated

--- backend/main.py
+++ backend/main.py
@@ -4,8 +4,20 @@ from fastapi import FastAPI
 from routes.doctors import doctors_router
 from routes.patients import patient_router
 from routes.diagnostics import diagnostics_router
+from fastapi.middleware.cors import CORSMiddleware
+from starlette.middleware import Middleware
 
-app = FastAPI()
+middleware = [
+    Middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_methods=["*"],
+        allow_credentials=True,
+        allow_headers=["*"],
+    ),
+]
+
+app = FastAPI(middleware=middleware)
 
 app.include_router(doctors_router, prefix="/doctors", tags=["doctors"])
 app.include_router(patient_router, prefix="/patients", tags=["patients"])
